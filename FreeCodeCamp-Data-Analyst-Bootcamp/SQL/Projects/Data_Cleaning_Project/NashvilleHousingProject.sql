d85893cb313297481591fa13ce3d6f71f77ba146
TehAutumnCore <naruto13581@gmail.com>
2025-06-29 12:59:03 -0400

    data cleaning

--- FreeCodeCamp-Data-Analyst-Bootcamp/SQL/Projects/Data_Cleaning_Project/NashvilleHousingProject.sql
+++ FreeCodeCamp-Data-Analyst-Bootcamp/SQL/Projects/Data_Cleaning_Project/NashvilleHousingProject.sql
@@ -1,25 +1,33 @@
-SELECT *
-FROM PortfolioProject..NashvilleHousing
-WHERE OwnerName IS NOT NULL
-ORDER BY UniqueID
-
-
 /*
 
 Cleaning Data in SQL Queries
 
 */
 
+SELECT *
+FROM PortfolioProject..NashvilleHousing
 
 
 --------------------------------------------------------------------------------------------------------------------------
 
 -- Standardize Date Format
+--Original data
+SELECT SaleDate, CONVERT(Date,SaleDate)
+FROM PortfolioProject..NashvilleHousing
 
+--Add a new column to store the cleaned date
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted DATE
 
+--update the new column with the converted date value
+UPDATE NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
 
+--preview the result
+SELECT SaleDate, SaleDateConverted
+FROM PortfolioProject..NashvilleHousing
 
- --------------------------------------------------------------------------------------------------------------------------
+ -------------------------------------------------------------------------------------------------------------------------
 
 -- Populate Property Address data
 
